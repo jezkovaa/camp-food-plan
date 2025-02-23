d08405cea49b92c1330eb363d8ccea7acc47de31
Alena Ježková <jezkoal1@fit.cvut.cz>
2025-02-23 11:46:53 +0100

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'camp.food.plan',
+  appName: 'camp-food-plan',
+  webDir: 'www'
+};
+
+export default config;
