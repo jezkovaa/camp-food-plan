c4ba5504d0bfa37663888cd4fa3b7fc4f5a1c439
Alena Ježková <jezkoal1@fit.cvut.cz>
2025-03-22 15:47:13 +0100

    closes #57 - planning home existing event

--- src/app/planning/ui/pages/planning.page/planning.page.ts
+++ src/app/planning/ui/pages/planning.page/planning.page.ts
@@ -1,17 +1,19 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonLabel, IonButton, IonIcon } from '@ionic/angular/standalone';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonLabel, IonButton, IonIcon, IonPopover } from '@ionic/angular/standalone';
 import { TranslateModule } from '@ngx-translate/core';
 import { addIcons } from 'ionicons';
-import { add } from 'ionicons/icons';
+import { add, chevronDown } from 'ionicons/icons';
 import { PlannedEvent } from 'src/app/planning/data/interfaces/planned-event.interface';
 import { PlanningService } from 'src/app/planning/data/services/planning.service';
+import { SelectEventComponent } from '../../components/select-popover/select-event.component';
+import { PopoverController } from '@ionic/angular';
 
 @Component({
   selector: 'app-planning',
   templateUrl: 'planning.page.html',
   styleUrls: ['planning.page.scss'],
-  imports: [
+  imports: [IonPopover,
     IonButton,
     IonLabel,
     IonHeader,
@@ -19,16 +21,27 @@ import { PlanningService } from 'src/app/planning/data/services/planning.service
     IonTitle,
     IonContent,
     IonIcon,
-
     CommonModule,
-    TranslateModule]
+    TranslateModule,
+    SelectEventComponent
+  ],
+  providers: [PopoverController]
 })
 export class PlanningPage implements OnInit {
 
   plannedEvents: PlannedEvent[] = [];
+  selectedEventId: string = '';
 
-  constructor(private planningService: PlanningService) {
-    addIcons({ add });
+  get selectedEventText(): string {
+    const selectedEvent = this.plannedEvents.find(event => event.id === this.selectedEventId);
+    return selectedEvent?.name ?? 'Select Event';
+  }
+
+  @ViewChild(IonPopover) popover!: IonPopover;
+
+  constructor(private planningService: PlanningService
+  ) {
+    addIcons({ add, chevronDown });
   }
 
   ngOnInit() {
@@ -44,5 +57,12 @@ export class PlanningPage implements OnInit {
 
   createEvent() {
     //todo
+  }
+
+  selectionChanged(selectedEventId: string) {
+    //todo redirect to event details
+    this.selectedEventId = selectedEventId;
+    this.popover.dismiss();
+
   }
 }
