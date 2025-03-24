2f3763d1f77303ffc9a25194bac6c09d0fd58f60
Alena Ježková <jezkoal1@fit.cvut.cz>
2025-03-24 11:00:29 +0100

    #4 - translations fixed

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,28 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
-import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
-import { TranslateModule } from '@ngx-translate/core';
+import { provideRouter } from '@angular/router';
+import { provideIonicAngular } from '@ionic/angular/standalone';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { routes } from './app/app.routes';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { AppComponent } from './app/app.component';
-import { provideHttpClient } from '@angular/common/http';
+import { HttpClient, provideHttpClient } from '@angular/common/http';
 import { importProvidersFrom } from '@angular/core';
+
+export function translateHttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, 'assets/i18n/', '.json');
+}
 
 bootstrapApplication(AppComponent, {
   providers: [
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    provideIonicAngular(),
     provideHttpClient(),
-    provideRouter(routes, withPreloading(PreloadAllModules)),
-    importProvidersFrom(TranslateModule.forRoot())
-
-  ],
+    provideRouter(routes),
+    provideIonicAngular(),
+    importProvidersFrom(TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: translateHttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })),
+  ]
 });
